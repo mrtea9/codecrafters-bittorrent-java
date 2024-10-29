ce758f211b7742a98771d172ab233de35cf4d63c
mrtea9 <ionmeriacri111@gmail.com>
2024-10-29 17:05:50 +0200

    Decode strings

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,12 +5,9 @@ public class Main {
   private static final Gson gson = new Gson();
 
   public static void main(String[] args) throws Exception {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.err.println("Logs from your program will appear here!");
-    
+
     String command = args[0];
     if("decode".equals(command)) {
-      //  Uncomment this block to pass the first stage
         String bencodedValue = args[1];
         String decoded;
         try {
@@ -29,15 +26,11 @@ public class Main {
 
   static String decodeBencode(String bencodedString) {
     if (Character.isDigit(bencodedString.charAt(0))) {
-      int firstColonIndex = 0;
-      for(int i = 0; i < bencodedString.length(); i++) { 
-        if(bencodedString.charAt(i) == ':') {
-          firstColonIndex = i;
-          break;
-        }
-      }
+      int firstColonIndex = bencodedString.indexOf(':');
       int length = Integer.parseInt(bencodedString.substring(0, firstColonIndex));
       return bencodedString.substring(firstColonIndex+1, firstColonIndex+1+length);
+    } else if (bencodedString.charAt(0) == 'i') {
+      return "sad";
     } else {
       throw new RuntimeException("Only strings are supported at the moment");
     }
