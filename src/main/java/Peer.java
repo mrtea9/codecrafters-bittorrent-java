59206475533b0e8b702c71ec87224b039c5e0e3b
mrtea9 <ionmeriacri111@gmail.com>
2024-10-31 14:15:24 +0200

    Discover peers

--- src/main/java/Peer.java
+++ src/main/java/Peer.java
@@ -1,8 +1,5 @@
 import java.io.IOException;
-import java.net.InetAddress;
-import java.net.InetSocketAddress;
-import java.net.URI;
-import java.net.URLEncoder;
+import java.net.*;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -55,8 +52,15 @@ public class Peer {
         try {
             HttpResponse<byte[]> response = client.send(request, HttpResponse.BodyHandlers.ofByteArray());
             System.out.println("status code: " + response.statusCode());
-            byte[] responseBodyBytes = response.body();
-            Map<String, Object> result = bencode.decode(responseBodyBytes, Type.DICTIONARY);
+            URL url = new URL(full_request);
+            HttpURLConnection connection =
+                    (HttpURLConnection)url.openConnection();
+            connection.setRequestMethod("GET");
+            int responseCode = connection.getResponseCode();
+            // System.out.println("responseCode = " + responseCode);
+            byte[] bytes = connection.getInputStream().readAllBytes();
+            Map<String, Object> result =
+                    bencode.decode(bytes, Type.DICTIONARY);
             ByteBuffer peersBuffer = (ByteBuffer)result.get("peers");
             System.out.println(gson.toJson(peersBuffer));
             System.out.println(gson.toJson(result));
