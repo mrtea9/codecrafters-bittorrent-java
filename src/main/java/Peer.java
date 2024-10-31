892bbec50b1c16f50d290dac4315fe98be94d21d
mrtea9 <ionmeriacri111@gmail.com>
2024-10-31 15:10:24 +0200

    Discover peers

--- src/main/java/Peer.java
+++ src/main/java/Peer.java
@@ -21,46 +21,53 @@ public class Peer {
         this.torrent = torrent;
     }
 
-    public String discoverPeers() {
+    public void discoverPeers() {
         String full_address = this.torrent.announce;
 
         performGetRequest();
-
-        return full_address;
     }
 
     public void performGetRequest() {
 
         HttpClient client = HttpClient.newHttpClient();
 
-        String peerId = "23141516167152146123";
-        String full_request = (
-                this.torrent.announce + "?info_hash="
-                + URLEncoder.encode(
-                        new String(this.torrent.infoHashBytes, StandardCharsets.ISO_8859_1),
-                        StandardCharsets.ISO_8859_1
-                )
-                + "&peer_id=" + peerId + "&port=6881&uploaded=0&downloaded=0&left=" + this.torrent.length
-                + "&compact=1"
-        );
-
-        HttpRequest request = HttpRequest.newBuilder(
-                URI.create(full_request))
-                .header("accept", "*/*")
-                .build();
+        HttpRequest request = createRequest();
 
         try {
             HttpResponse<byte[]> response = client.send(request, HttpResponse.BodyHandlers.ofByteArray());
+
             System.out.println("status code: " + response.statusCode());
+
             byte[] responseBodyBytes = response.body();
-            Map<String, Object> result = bencode.decode(responseBodyBytes, Type.DICTIONARY);
-            ByteBuffer peersBuffer = (ByteBuffer)result.get("peers");
+            Map<String, Object> decodedResponse = bencode.decode(responseBodyBytes, Type.DICTIONARY);
+
+            ByteBuffer peersBuffer = (ByteBuffer)decodedResponse.get("peers");
             byte[] peersBytes = Torrent.ByteBufferToBytes(peersBuffer);
 
             printIpFromBytes(peersBytes);
         } catch (InterruptedException | IOException e) {
             System.out.println(e.getMessage());
         }
+    }
+
+    private HttpRequest createRequest() {
+        String peerId = "23141516167152146123";
+        String full_request = (
+                this.torrent.announce + "?info_hash="
+                        + URLEncoder.encode(
+                        new String(this.torrent.infoHashBytes, StandardCharsets.ISO_8859_1),
+                        StandardCharsets.ISO_8859_1
+                )
+                        + "&peer_id=" + peerId + "&port=6881&uploaded=0&downloaded=0&left=" + this.torrent.length
+                        + "&compact=1"
+        );
+
+        HttpRequest request = HttpRequest.newBuilder(
+                        URI.create(full_request))
+                .header("accept", "*/*")
+                .build();
+
+        return request;
     }
 
     private static void printIpFromBytes(byte[] bytes) throws UnknownHostException {
