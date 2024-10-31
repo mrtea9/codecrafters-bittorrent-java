d44641dae0774211c0f440fc7804b291a868dc0c
mrtea9 <ionmeriacri111@gmail.com>
2024-10-31 12:21:39 +0200

    Discover peers

--- src/main/java/Peer.java
+++ src/main/java/Peer.java
@@ -55,7 +55,7 @@ public class Peer {
             byte[] responseBodyBytes = response.body();
             Map<String, Object> result = bencode.decode(responseBodyBytes, Type.DICTIONARY);
             System.out.println(result.toString());
-            System.out.println(gson.toJson(result));
+            System.out.println(gson.toJson(result.get("peers")));
         } catch (InterruptedException | IOException e) {
             System.out.println(e.getMessage());
         }
